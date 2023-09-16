a943503ba3552de562625c5ab7db6f215706b2e8
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-09-16 13:12:52 -0700

    login page ui

---
+++ lib/screen/login_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class LoginScreen extends StatefulWidget {
+  final VoidCallback show;
+  LoginScreen(this.show, {super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final email = TextEditingController();
+  FocusNode email_F = FocusNode();
+  final password = TextEditingController();
+  FocusNode password_F = FocusNode();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(width: 96.w, height: 100.h),
+            Center(
+              child: Image.asset('images/logo.jpg'),
+            ),
+            SizedBox(height: 120.h),
+            Textfild(email, email_F, 'Email', Icons.email),
+            SizedBox(height: 15.h),
+            Textfild(password, password_F, 'Password', Icons.lock),
+            SizedBox(height: 15.h),
+            forget(),
+            SizedBox(height: 15.h),
+            login(),
+            SizedBox(height: 15.h),
+            Have()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget Have() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Text(
+            "Don't have account?  ",
+            style: TextStyle(
+              fontSize: 14.sp,
+              color: Colors.grey,
+            ),
+          ),
+          GestureDetector(
+            onTap: widget.show,
+            child: Text(
+              "Sign up ",
+              style: TextStyle(
+                  fontSize: 15.sp,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget login() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        alignment: Alignment.center,
+        width: double.infinity,
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          borderRadius: BorderRadius.circular(10.r),
+        ),
+        child: Text(
+          'Login',
+          style: TextStyle(
+            fontSize: 23.sp,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding forget() {
+    return Padding(
+      padding: EdgeInsets.only(left: 230.w),
+      child: GestureDetector(
+        onTap: () {},
+        child: Text(
+          'Forgot password?',
+          style: TextStyle(
+            fontSize: 13.sp,
+            color: Colors.blue,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding Textfild(TextEditingController controll, FocusNode focusNode,
+      String typename, IconData icon) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w),
+      child: Container(
+        height: 44.h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(5.r),
+        ),
+        child: TextField(
+          style: TextStyle(fontSize: 18.sp, color: Colors.black),
+          controller: controll,
+          focusNode: focusNode,
+          decoration: InputDecoration(
+            hintText: typename,
+            prefixIcon: Icon(
+              icon,
+              color: focusNode.hasFocus ? Colors.black : Colors.grey[600],
+            ),
+            contentPadding:
+                EdgeInsets.symmetric(horizontal: 15.w, vertical: 15.h),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(
+                width: 2.w,
+                color: Colors.grey,
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(5.r),
+              borderSide: BorderSide(
+                width: 2.w,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
