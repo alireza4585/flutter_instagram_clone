b3254e0f8bfb88a8badc1d6d3f95f576d458bf47
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-09-24 01:11:16 -0700

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_instagram_clone/auth/auth_screen.dart';
-import 'package:flutter_instagram_clone/screen/login_screen.dart';
+import 'package:flutter_instagram_clone/firebase_options.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
