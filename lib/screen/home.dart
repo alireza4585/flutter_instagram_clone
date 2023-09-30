9dccd8e441e21044afdc47c6c0e67531ead52a50
Alireza Mohammadian <alirezamohammadian@Alirezas-Mac-mini.local>
2023-09-30 11:42:06 -0700

    home screen  app bar

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,12 +13,25 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-            onTap: () {
-              FirebaseAuth.instance.signOut();
-            },
-            child: Text('Home Screen')),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        title: SizedBox(
+          width: 105.w,
+          height: 28.h,
+          child: Image.asset('images/instagram.jpg'),
+        ),
+        leading: Image.asset('images/camera.jpg'),
+        actions: [
+          const Icon(
+            Icons.favorite_border_outlined,
+            color: Colors.black,
+            size: 25,
+          ),
+          Image.asset('images/send.jpg'),
+        ],
+        backgroundColor: const Color(0xffFAFAFA),
       ),
     );
   }
